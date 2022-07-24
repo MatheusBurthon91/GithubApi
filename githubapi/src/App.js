82178fb5bfef133b3127c8a427534565dfcca662
Matheus Burthon <matatiaspatrizio@hotmail.com>
2022-07-24 19:54:49 -0300

    add: feito um componente header para inicar a estilizaçao da pagina.

--- githubapi/src/App.js
+++ githubapi/src/App.js
@@ -1,4 +1,6 @@
 // eslint-disable-next-line import/no-unresolved
+import Header from "./components/Header";
+// eslint-disable-next-line import/no-unresolved
 import ScreenRepositories from "./components/ScreenRepositories";
 // eslint-disable-next-line import/no-unresolved
 import ScreenUserData from "./components/ScreenUserData";
@@ -8,6 +10,7 @@ import SearchBar from "./components/SearchBar";
 export default function App() {
   return (
     <main>
+      <Header />
       <SearchBar />
       <ScreenUserData />
       <ScreenRepositories />
---
+++ githubapi/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return(
+  <header>
+    <img 
+      src="https://i.pinimg.com/736x/b1/5e/ed/b15eedbdafbbdbca3249e3942f4faf3b.jpg"
+      alt="github-logo" 
+    />
+  </header>
+  )
+}
