9941e31ecdeb7a646b2acb70e63ffb1cd53e4790
Matheus Burthon <matatiaspatrizio@hotmail.com>
2022-07-24 19:41:02 -0300

    del: Removendo a pasta de testes.

--- githubapi/src/tests/Screen.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-
-test("teste inicial", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Pesquisar Repositorios/i);
-  expect(linkElement).toBeInTheDocument();
-});
