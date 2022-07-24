f2f9ea110261e63bef23ee06d133224c47c0dee7
Matheus Burthon <matatiaspatrizio@hotmail.com>
2022-07-24 14:58:24 -0300

    add: adicionando estados iniciais do input e do botao no provider.

--- githubapi/src/helpers/Provider.jsx
+++ githubapi/src/helpers/Provider.jsx
@@ -1,10 +1,21 @@
 // eslint-disable-next-line import/no-unresolved
 import AppContext from './GlobalContext';
+import { useState } from 'react';
 
 export default function Provider({ children }) {
+  const [input, setInput] = useState('');
+  const [accessButton, setAccessButton] = useState(true); 
+
+  const values = {
+    input, 
+    setInput,
+    accessButton,
+    setAccessButton,
+  }
+
   return (
-    <AppContext.Provider>
-      {children}
+    <AppContext.Provider value={ values }>
+      { children }
     </AppContext.Provider>
   );
 }
