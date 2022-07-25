2b91ab7c2da8760ea6f003cada0a5e76c4d5b79d
Matheus Burthon <matatiaspatrizio@hotmail.com>
2022-07-24 22:31:00 -0300

    refactor: removendo div desnecessaria, e alterando a imagem e cor no componente header.

--- githubapi/src/components/Header.jsx
+++ githubapi/src/components/Header.jsx
@@ -3,13 +3,11 @@ import "../style.css";
 export default function Header() {
   return (
   <header className="header-center">
-    <div>
     <img
       className="logo-git"
       src="https://icons.iconarchive.com/icons/limav/flat-gradient-social/512/Github-icon.png"
       alt="github-logo" 
     />
-    </div>
   </header>
   );
 }
