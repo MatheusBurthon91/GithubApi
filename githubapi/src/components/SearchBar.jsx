ac00c4c3b4a80253780c15b4f4e4f491bcaa6362
Matheus Burthon <matatiaspatrizio@hotmail.com>
2022-07-24 23:13:21 -0300

    add: adicionando o css no componente SearchBar.

--- githubapi/src/components/SearchBar.jsx
+++ githubapi/src/components/SearchBar.jsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect } from "react";
 // eslint-disable-next-line import/no-unresolved
 import GlobalContext from "../helpers/GlobalContext";
+import "../style.css";
+
 
 export default function SearchBar() {
   const {
@@ -33,25 +35,31 @@ export default function SearchBar() {
   }, [searchInput, accessButton]);
 
   return (
-    <section>
+    <section className="search-bar">
+      <div>
       <h1>
       User Finder: seu pesquisador de repositórios publicos de qualquer usuário
       </h1>
+      <div className="in-search">
       <label htmlFor="user">
         Usuário:
         <input
           type="text"
           onChange={({ target }) => setSearchInput(target.value)}
-          placeholder="Digite o usário"
+          placeholder="Digite o usuário"
+          className="input-search"
         />
       </label>
       <button
         type="button"
         disabled={ accessButton }
         onClick={() => searchRepositoriesByUser() }
+        className="button-search"
       >
-          Pesquisar Repostorios
+          Submeter
       </button>
+      </div>
+      </div>
     </section>
   );
 }
