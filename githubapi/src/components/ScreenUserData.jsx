9e5476e9d4754b3a7ac7e0b171de684b6ca466f2
Matheus Burthon <matatiaspatrizio@hotmail.com>
2022-07-24 23:48:56 -0300

    add: feito o css do componente screenUserData.

--- githubapi/src/components/ScreenUserData.jsx
+++ githubapi/src/components/ScreenUserData.jsx
@@ -1,21 +1,24 @@
 import { useContext } from "react";
 // eslint-disable-next-line import/no-unresolved
 import GlobalContext from "../helpers/GlobalContext";
+import "../style.css";
 
 
 export default function ScreenUserData() {
   const { user } = useContext(GlobalContext);
 
   return (
-    <section>
+    <section className="center-user-info">
       {
         Object.keys(user).length === 0 ? (<p>Sem usuário, pesquise por um no input acima.</p>) :
-        (<div>
-          <h3>{user.login}</h3>
-            <img src={user.avatar_url} alt={user.login} />
-            <a href={user.html_url} target="_blank" rel="noreferrer" id="profile">
-              Link do perfil
-            </a>
+        (<div className="controller-user">
+            <p className="fontSize-description">{user.login}</p>
+            <img src={user.avatar_url} alt={user.login} className="img-user" />
+            <p className="fontSize-description">
+              <a href={user.html_url} target="_blank" rel="noreferrer" id="profile">
+                Link do perfil
+              </a>
+            </p>
         </div>
         )}
     </section>
