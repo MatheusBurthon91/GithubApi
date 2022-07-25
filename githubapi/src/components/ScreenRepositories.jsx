81ec5961ed2988254f967c1ad57883d2803931a6
Matheus Burthon <matatiaspatrizio@hotmail.com>
2022-07-24 23:57:26 -0300

    add: adicionando classe em cada repositorio no componente screenRepositories.

--- githubapi/src/components/ScreenRepositories.jsx
+++ githubapi/src/components/ScreenRepositories.jsx
@@ -1,17 +1,19 @@
 import { useContext } from "react";
 // eslint-disable-next-line import/no-unresolved
 import GlobalContext from "../helpers/GlobalContext";
+import "../style.css";
+
 
 export default function ScreenRepositories() {
   const { repositories } = useContext(GlobalContext);
 
   return (
-    <section>
+    <section className="center-user-info">
       {!repositories.length ? (
       <h1>
         Sem repositorios nessa seção pesquisa no input acima
       </h1>) : (
-        <div>
+        <div className="one-repositore">
           {repositories.map((repository) => (
             <div key={repository.id}>
               <h3>{repository.name}</h3>
